438aa2a800c2e1d99b3ca9f8ea1156d90e5eb2c5
okyiww <okyiww@gmail.com>
2025-10-19 00:27:29 +0800

    chore: update playground flow

--- packages/server/src/configs/index.ts
+++ packages/server/src/configs/index.ts
@@ -1,5 +1,5 @@
 export const configs = {
-  basePath: process.env.BASE_PATH || "/todo",
+  basePath: process.env.BASE_PATH || "/form-playground",
   databaseUrl:
     process.env.DATABASE_URL ||
     "postgres://username:password@localhost:9999/formserver",
