6d15042ea9e1e372836871ddc787d181172e089d
okyiww <okyiww@gmail.com>
2025-11-14 00:05:23 +0800

    feat: bpm try remove model

--- packages/form/src/core/lifecycle/hooks/useSPTypeHandler.ts
+++ packages/form/src/core/lifecycle/hooks/useSPTypeHandler.ts
@@ -1,8 +1,7 @@
 import { FormContext } from "@/core/context";
 
 export function useSPTypeHandler(value: any) {
-  return async ({ model }) => {
-    console.log("model", model.name);
-    return await FormContext.ssr[value.__sp_type__]({ model, ...value });
+  return async () => {
+    return await FormContext.ssr[value.__sp_type__]({ ...value });
   };
 }
