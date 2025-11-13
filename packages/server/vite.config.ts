8078d8221aaac0613c59c7677aeddad229882a43
okyiww <okyiww@gmail.com>
2025-11-13 16:15:13 +0800

    fix: use vite

---
+++ packages/server/vite.config.ts
@@ -0,0 +1,49 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig(({ mode }) => ({
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: resolve(__dirname, "./src"),
+      },
+    ],
+  },
+  build: {
+    ssr: true,
+    lib: {
+      entry: resolve(__dirname, "./src/index.ts"),
+      name: "lib",
+      fileName: "index",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+      format: {
+        comments: false,
+      },
+    },
+    sourcemap: mode === "development" ? "inline" : false,
+  },
+  plugins: [
+    dts({
+      cleanVueFileName: true,
+      outDir: "dist/types",
+      include: ["./**/*"],
+      exclude: ["**/*.spec.ts", "**/*.spec.tsx"],
+    }),
+  ],
+}));
